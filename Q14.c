93844906f1e66283b38804fa9c03540e2e717a84
prafullaspi11504706 <31003549+prafullaspi11504706@users.noreply.github.com>
2019-04-10 00:08:52 +0530

    Update Q14.c

--- Q14.c
+++ Q14.c
@@ -5,7 +5,7 @@ int main()
 
 {
 
-int bt[20],p[20],wt[20],tat[20],pr[20],i,j,n,total=0,pos,temp,avg_wt,avg_tat;
+int bt[20],p[20],wt[20],tat[20],pr[20],i,j,n,total=0,pos,temp,avg_wt,avg_tat,x;
   
 printf("Enter Total Number of Process:");
 
